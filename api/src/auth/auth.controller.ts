2093e49491305e0a30ade65fa7fcc254aa3f66b4
bogosbalint <bogosbalint2001@gmail.com>
2022-10-19 15:13:05 +0200

    Protected User and Auth endpoints, and implemented update profile

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Patch, Post, UseGuards } from '@nestjs/common';
 import { ExistingUserDTO } from 'src/user/dto/existing-user.dto';
 import { NewUserDTO } from 'src/user/dto/new-user.dto';
 import { IUser } from 'src/user/user.interface';
 import { UserDocument } from 'src/user/user.schema';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
+import { Roles } from './decorators/roles.decorator';
 import { JwtGuard } from './guards/jwt.guard';
+import { RoleGuard } from './guards/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -22,9 +24,17 @@ export class AuthController {
         return this.authService.login(user);
     }
 
-    @UseGuards(JwtGuard)
+    @Roles('user', 'admin')
+    @UseGuards(JwtGuard, RoleGuard)
     @Get("profile")
     profile(@CurrentUser('email') email: string): Promise<UserDocument> {
-        return this.authService.findByEmail(email);
+        return this.authService.profile(email);
+    }
+
+    @Roles('user', 'admin')
+    @UseGuards(JwtGuard, RoleGuard)
+    @Patch()
+    update(@CurrentUser('id') id: string, @Body('username') username: string): Promise<UserDocument> {
+        return this.authService.update(id, username);
     }
 }
