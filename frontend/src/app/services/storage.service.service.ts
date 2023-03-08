f4733d01e1522351b3d4effddc32d7040247303b
bogosbalint <bogosbalint2001@gmail.com>
2023-03-08 12:34:46 +0100

    Created storage service

---
+++ frontend/src/app/services/storage.service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageServiceService {
+
+  constructor() { }
+
+  async set(key: string, value: string) {
+    await localStorage.setItem(key, value);
+  }
+
+  async get<T = any>(key: string): Promise<T | string> {
+    return (await localStorage.getItem(key)) ?? '';
+  }
+
+  public remove(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public clear() {
+    localStorage.clear();
+  }
+}
